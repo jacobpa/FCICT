35dc8760886e745b4222be0bfcb97d9da067a44b
Jacob Patterson <jacob@jacobpa.com>
2020-04-05 22:54:02 -0400

    Update index + styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,16 @@
 <script>
+import axios from 'axios';
 import Header from '../components/Header.svelte';
+import Card from '../components/Card.svelte';
+import ChartCard from '../components/ChartCard.svelte';
+
+let request;
+const getData = (groupBy) => {
+    request = axios.get('http://192.168.2.11:3000/api/all?groupBy=' + groupBy);
+}
+
+getData('totals');
 </script>
 
 <Header />
+<ChartCard dataRequest={request}/>
