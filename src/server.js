e27c58fac242d20df6c01c72f9c7635a88e63855
Jacob Patterson <jacob@jacobpa.com>
2020-04-06 17:01:44 -0400

    Update server to include API

--- src/server.js
+++ src/server.js
@@ -3,15 +3,31 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+import apiRouter from './server/api';
+import FCICSDb from './db';
+
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
+const app = polka();
+
+const init = async() => {
+	const db = await FCICSDb.getDatabase();
+
+	app.use((req, res, next) => {
+		req.db = db;
+		next();
+	});
+	
+	app.use(compression({threshold: 0}));
+	app.use(sirv('static', { dev }));
+	app.use('/api', apiRouter);
+	app.use(sapper.middleware());
+
+
+	app.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
+}
+
+init();
