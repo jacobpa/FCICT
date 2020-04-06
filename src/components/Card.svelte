d4b51e0c3c81dc765b934abcdd46bb761af889ce
Jacob Patterson <jacob@jacobpa.com>
2020-04-06 19:07:19 -0400

    Update card component to include title/body slots

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,15 +1,20 @@
+<script>
+export let hasTitle = true;
+</script>
+
 <style>
 section {
   background-color: white;
   border-radius: 8px;
   padding: 1rem;
   margin-bottom: 1rem;
-  color: black;
+  color: #33302e;
   border: 1px solid black;
   box-shadow: 3px 4px 0 black;
 }
 </style>
 
 <section>
-<slot></slot>
+{#if hasTitle}<h2 class='title'><slot name="title"></slot></h2>{/if}
+<slot name="body"></slot>
 </section>
