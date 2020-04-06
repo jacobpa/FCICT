27dafd04d6058789ceeb286f7080358be2da6796
Jacob Patterson <jacob@jacobpa.com>
2020-04-06 00:10:36 -0400

    Add to global styles

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -3,6 +3,7 @@ section {
   background-color: white;
   border-radius: 8px;
   padding: 1rem;
+  margin-bottom: 1rem;
   color: black;
   border: 1px solid black;
   box-shadow: 3px 4px 0 black;
--- src/components/ChartCard.svelte
+++ src/components/ChartCard.svelte
@@ -39,6 +39,7 @@
     background-color: white;
     border-radius: 8px;
     padding: 1rem;
+    margin-bottom: 1rem;
     color: black;
     border: 1px solid black;
     box-shadow: 3px 4px 0 black;
