29e1b4fd76dd517f24d758b1515743640f6d80fb
Jacob Patterson <jacob@jacobpa.com>
2020-04-05 20:18:53 -0400

    Add Header component, update Index

---
+++ src/components/Header.svelte
@@ -0,0 +1,34 @@
+<style>
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2em;
+    text-align: center;
+  }
+
+  header .title {
+    display: block;
+    text-transform: uppercase;
+    color: white;
+    font-size: 3.75rem;
+    font-weight: 900;
+    text-shadow: -1px -1px 0 black, -1px 1px 0 black, 1px -1px 0 black,
+      1px 1px 0 black, 2px 3px 0 black;
+  }
+
+  header .subtitle {
+    display: block;
+    width: 90%;
+    font-weight: 400;
+    font-style: italic;
+    font-size: 1.1rem;
+  }
+</style>
+
+<header>
+  <span class="title">FCICT</span>
+  <span class="subtitle">
+    Franklin County Inmate population and COVID-19 case Tracker
+  </span>
+</header>
