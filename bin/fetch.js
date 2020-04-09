26bdf4f25bb01e51f5d6f1f6436abed5d56ba2b3
Jacob Patterson <jacob@jacobpa.com>
2020-04-09 19:41:59 -0400

    Refactor fetch script for use with node-cron

--- bin/fetch.js
+++ bin/fetch.js
@@ -1,34 +1,3 @@
-import moment from 'moment';
-import FCICS from 'FCICS';
-import FCICSDb from '../src/db/index';
+import fetch from '../src/jobs/fetch';
 
-
-const fetchAndStoreData = async () => {
-  const db = await FCICSDb.getDatabase();
-  const cc = new FCICS.CovidClient();
-  const jc = new FCICS.JailClient();
-  const now = moment().format('YYYY-MM-DD HH:mm:ss');
-
-  try {
-    console.log('Fetching inmate population data...');
-
-    const inmates = {
-      male: await jc.search({ gender: 'M' }),
-      female: await jc.search({ gender: 'F' }),
-    };
-    db.insertInmates(now, inmates.male, inmates.female);
-  } catch (err) {
-    console.error(`Error pushing inmate data to DB: ${err.message}`);
-  }
-
-  try {
-    console.log('Fetching COVID-19 case data...');
-
-    const cases = await cc.get();
-    db.insertCovid(now, cases.male, cases.female);
-  } catch (err) {
-    console.error(`Error pushing COVID-19 data to DB: ${err.message}`);
-  }
-};
-
-fetchAndStoreData();
+fetch();
---
+++ src/jobs/fetch.js
@@ -0,0 +1,33 @@
+import moment from 'moment';
+import FCICS from 'FCICS';
+import FCICSDb from '../db/index';
+
+const fetchAndStoreData = async () => {
+  const db = await FCICSDb.getDatabase();
+  const cc = new FCICS.CovidClient();
+  const jc = new FCICS.JailClient();
+  const now = moment().format('YYYY-MM-DD HH:mm:ss');
+
+  try {
+    console.log('Fetching inmate population data...');
+
+    const inmates = {
+      male: await jc.search({ gender: 'M' }),
+      female: await jc.search({ gender: 'F' }),
+    };
+    db.insertInmates(now, inmates.male, inmates.female);
+  } catch (err) {
+    console.error(`Error pushing inmate data to DB: ${err.message}`);
+  }
+
+  try {
+    console.log('Fetching COVID-19 case data...');
+
+    const cases = await cc.get();
+    db.insertCovid(now, cases.male, cases.female);
+  } catch (err) {
+    console.error(`Error pushing COVID-19 data to DB: ${err.message}`);
+  }
+};
+
+export default fetchAndStoreData;
