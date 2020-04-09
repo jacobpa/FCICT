d5f1619dbedf48639f387b08edc339a529871330
Jacob Patterson <jacob@jacobpa.com>
2020-04-09 19:42:20 -0400

    Add node-cron and set up fetch job

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import cron from 'node-cron';
 
 import apiRouter from './server/api';
 import FCICSDb from './db';
+import fetch from './jobs/fetch';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -13,6 +15,10 @@ const app = polka();
 
 const init = async () => {
   const db = await FCICSDb.getDatabase();
+
+  cron.schedule('15 2,8,14,20 * * *', () => {
+    fetch();
+  }, { timezone: 'America/New_York' });
 
   app.use((req, res, next) => {
     req.db = db;
